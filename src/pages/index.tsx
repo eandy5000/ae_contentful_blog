5bc35bfe93eef85173abf7780ec5e133d62b7e2b
Andy <eandy5000@yahoo.com>
2019-08-03 09:46:43 -0500

    added graphql query and log

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = (): JSX.Element => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+const IndexPage = ({ data }: any): JSX.Element => {
+  console.log("data", data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
 
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+export const query = graphql`
+  query getAllStories {
+    allContentfulStory {
+      edges {
+        node {
+          id
+          name
+          hasVideo
+          tags
+          url
+          createdAt
+          publisher {
+            id
+            name
+            createdAt
+            homeUrl
+            homeCountry
+          }
+        }
+      }
+    }
+  }
+`
